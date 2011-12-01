c303b4b236f048e324ec7d496eb4e4646d46e8b1
Jan Haag <haag498@gmail.com>
2011-12-01 18:52:44 +0100

    Slideset 6

---
+++ Tut6/Aufgabe/LinkedPointList.java
@@ -0,0 +1,51 @@
+public class LinkedPointList {
+    public void add(Point p, int index) {
+    }
+
+    public void add(Point p) {
+    }
+
+    public void addFirst(Point p) {
+    }
+
+    public void addLast(Point p) {
+    }
+
+    public boolean contains(Point p) {
+    }
+
+    public Point get(int index) {
+    }
+
+    public Point getFirst() {
+    }
+
+    public Point getLast() {
+    }
+
+    public int indexOf(Point p) {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public void remove(int index) {
+    }
+
+    public void remove(Point p) {
+    }
+
+    public void removeFirst() {
+    }
+
+    public void removeLast() {
+    }
+
+    public int size() {
+    }
+
+    //Optional:
+    public void sortByDistanceToOrigin() {
+    }
+}
+
